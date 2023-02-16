e53d6ecba04accb42fd2347255a2f93002650c38
hurocup_black <humanoidgithub@gmail.com>
2023-02-16 20:31:11 +0800

    Chia-Yu:調整步態,調整修整角度程式

--- src/strategy/Kidsize_HuroCup/robot2.0.py
+++ src/strategy/Kidsize_HuroCup/robot2.0.py
@@ -13,30 +13,36 @@ strategy = False    #第一次指撥flag
 forward = 0         #前進flag
 mode = 1            #目標模式
 head = 2047         #頭部馬達初始角度
-speed = 5500        #前進初速度
-bspeed1 = -4000     #後退初速度
-max_speed = 5500    #前進最快速度
+speed = 3000        #前進初速度
+bspeed1 = -3000     #後退初速度
+max_speed = 4000    #前進最快速度
 min_speed = 3000    #減速最慢速度  
-max_bspeed = -7000  #後退最快速度
+max_bspeed = -4000  #後退最快速度ss
 speed_add = 200     #前進增加量
 speed_sub = 300     #前進減速量
 bspeed_add = 200    #後退增加量
 theta = 0           #副函式進退YAW值調整
-thetafix=1        #前進YAw值補償
-thetafixb=-1       #後退YAw值補償
-target = 8000       #目標面積
+thetafix=0        #前進YAw值補償
+thetafixb=-2       #後退YAw值補償   #-3會偏左  -2微微偏右
+target = 8300       #目標面積
 
 def yaw_forward(y): #前進YAW值調整
     global yaw_hold
     global theta
-    if(y)>yaw_hold+3:
-      print("----->>>>>")
-      print(y)
-      theta=-2 
-    elif(y)<yaw_hold-3:
-      print("<<<<<-----")
-      print(y)
-      theta=2
+    if(y)>yaw_hold+4:
+      if(theta>0):
+        theta=0
+      else:
+        print("turn right")
+        print(y)
+        theta=-1 
+    elif(y)<yaw_hold-4:
+      if(theta<0):
+        theta=0
+      else:
+        print("turn left")
+        print(y)
+        theta=2
     else:
       theta=0
     return theta
@@ -45,13 +51,19 @@ def yaw_backward(by): #後退YAW值調整
     global yaw_hold
     global theta
     if(by)>yaw_hold+3:
-      print("<<<<<-----")
-      print(by)
-      theta=-2
+      if(theta>0):
+        theta=0
+      else:
+        print("<<<<<-----")
+        print(by)
+        theta=-3
     elif(by)<yaw_hold-3:
-      print("----->>>>>")
-      print(by)
-      theta=2
+      if(theta<0):
+        theta=0
+      else:
+        print("----->>>>>")
+        print(by)
+        theta=1
     else:
       theta=0
     return theta 
@@ -188,11 +200,11 @@ def initial():    #初始化
   head=2047
   yaw_start=0
   color1=100
-  speed=5500
+  speed=3000
   min_speed=3000
   speed1=0
   bspeed=0
-  bspeed1=-4000
+  bspeed1=-3000
   headangle=0
   forward=0
   yaw_hold=0
@@ -272,6 +284,9 @@ if __name__ == '__main__':
             initial()
             send.sendHeadMotor(2,2047,100)
             strategy=False
+            time.sleep(0.1)
+            send.sendBodySector(999)
+            time.sleep(0.1)
       
   except rospy.ROSInterruptException:
         pass

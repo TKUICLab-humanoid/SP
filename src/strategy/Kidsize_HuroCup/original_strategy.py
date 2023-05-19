6c242c9b4ece68f338657b7fa770a9472d580edf
hurocup_[AR] <humanoidgithub@gmail.com>
2023-05-19 11:40:46 +0800

    forward 18s back 30s total 48s

--- src/strategy/Kidsize_HuroCup/original_strategy.py
+++ src/strategy/Kidsize_HuroCup/original_strategy.py
@@ -8,12 +8,12 @@ import time
 FORWARD_START_SPEED = 10000
 BACK_START_SPEED = -4000
 FORWARD_MIN_SPEED = 6000
-BACK_MAX_SPEED = -10000
+BACK_MAX_SPEED = -8500
 FORWARD_ORIGIN_THETA = 0
 BACK_ORIGIN_THETA = 0
 TARGET_Y = 130
 OBJECT_MIN_SIZE = 1000
-DELAY_TIME = 0.5
+DELAY_TIME = 1.5
 COLOR_DICT = {  'Orange': 0,
                 'Yellow': 1,
                 'Blue':   2,
@@ -58,7 +58,6 @@ class Sprint:
             time.sleep(0.05)
             send.sendContinuousValue(self.speed, 0, 0, self.theta, 0)
             rospy.loginfo(f'Walk Status : {self.walk_status}')
-            rospy.logdebug(f'Speed : {self.speed}, Theta : {self.theta}')
             rospy.loginfo(f'Yaw : {self.yaw}, Pitch : {send.imu_value_Pitch}')
         if not send.is_start:
             if self.walk_status != 'First':
@@ -69,36 +68,34 @@ class Sprint:
                 self.init()
         
     def forward_control(self): #前進控制
-        if -8 <= self.yaw <= 8:
+        if -6 <= self.yaw <= 8:
             self.theta = FORWARD_ORIGIN_THETA
         else:
             self.theta = -2 + FORWARD_ORIGIN_THETA if self.yaw > 0 else 2 + FORWARD_ORIGIN_THETA
         self.speed = max(FORWARD_MIN_SPEED, self.speed + self.speed_add)
         
     def back_control(self): #後退控制
-        if -8 <= self.yaw <= 8:
+        if -6 <= self.yaw <= 8:
             self.theta = FORWARD_ORIGIN_THETA
         else:
             self.theta = -2 + FORWARD_ORIGIN_THETA if self.yaw > 0 else 2 + FORWARD_ORIGIN_THETA
-        if time.time() - self.speed_add_delay >= 0.1 : 
+        if time.time() - self.speed_add_delay >= DELAY_TIME : 
             self.speed = max(BACK_MAX_SPEED, self.speed + self.speed_add)
-            self.speed_add_delay = time.time()        
         
     def find_target(self):  #找目標物     
         target_y = 0 
         left_object_index = self.get_object(self.left_object_color, 1, 255, 0, 0)
         right_object_index = self.get_object(self.right_object_color, 2, 0, 0, 255)
-        rospy.logdebug(f'Left : {left_object_index}, Right : {right_object_index}')
         if left_object_index != None and right_object_index != None:
             if send.color_mask_subject_XMin[self.right_object_color][right_object_index] > send.color_mask_subject_XMin[self.left_object_color][left_object_index] and send.color_mask_subject_XMax[self.right_object_color][right_object_index] > send.color_mask_subject_XMax[self.left_object_color][left_object_index]:
                 target_y =  (int(send.color_mask_subject_Y[self.left_object_color][left_object_index] + send.color_mask_subject_Y[self.right_object_color][right_object_index]) / 2)
-        rospy.logdebug(f"Target Y : {target_y}")
+        rospy.loginfo(f"Target Y : {target_y}")
         if target_y >= 40:
             self.speed_add = -100
             if target_y >= TARGET_Y:
                 self.speed = BACK_START_SPEED
                 self.walk_status = 'Back'
-                self.speed_add_delay = time.time() - DELAY_TIME
+                self.speed_add_delay = time.time()
 
     def get_object(self, color, cnt, r, g, b):
         max_object = max(send.color_mask_subject_size[color])
